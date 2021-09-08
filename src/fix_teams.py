a9667c3e96039e59f9c5874aff38f26d768b54e4
Larry Shields <larry.shields@gmail.com>
2021-09-08 19:52:18 +0000

    Added Script to updated Home/Away Teams

---
+++ src/fix_teams.py
@@ -0,0 +1,41 @@
+import json
+import os
+
+
+source_folder = 'input'
+output_folder = 'output'
+
+files = os.listdir(source_folder)
+
+for file in files:
+  with open(source_folder + '/' + file, 'r') as input_file:
+    data = input_file.read()
+    json_data = json.loads(data)
+
+    if not 'awayteam' in json_data:
+      print(file + ' missing team')
+      bigplays = json_data['bigplays']
+      teams = []
+      for play in bigplays:
+        if not play['team'] in teams:
+          teams.append(str(play['team']))
+      teams.remove(str(json_data['hometeam']))
+      json_data['awayteam'] = teams[0]
+      print(teams[0] + ' set as hometeam for ' + file)
+
+
+    if not 'hometeam' in json_data:
+      print(file + ' missing team')
+      bigplays = json_data['bigplays']
+      teams = []
+      for play in bigplays:
+        if not play['team'] in teams:
+          teams.append(str(play['team']))
+      teams.remove(str(json_data['awayteam']))
+      json_data['hometeam'] = teams[0]
+      print(teams[0] + ' set as awayteam for ' + file)
+
+    print('Outputing file: ' + file)
+    with open(output_folder + '/' + file, 'w') as output_file:
+      output_file.write(json.dumps(json_data))
+print('Done')
